686c58c2271910ece54c20289afb552380fbaae5
2TomL <122866126+2TomL@users.noreply.github.com>
2023-01-17 14:05:13 +0100

    GitHub solo save

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -3,5 +3,6 @@ package be.intecbrussel;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Yo Yo World");
+        System.out.println("We got the GITHub WOOOOOO");
     }
 }
